03f5928018d48cd1e7558cff5a9465133ddc8d04
iceStorm <hauduedaiduong@gmail.com>
2022-04-01 22:18:35 +0700

    add user.controller method

--- src/app/app.controller.ts
+++ src/app/app.controller.ts
@@ -2,11 +2,16 @@ import { Request, Response } from "express"
 import { Controller as Controller } from "src/core/decorators"
 import { Get } from "src/core/decorators/http.decorator"
 
-@Controller("/")
+@Controller("")
 export default class AppController {
     @Get("/")
     index(req: Request, res: Response) {
         res.status(200).send("Ok")
+    }
+
+    @Get("/:number")
+    number(req: Request, res: Response) {
+        res.status(200).send(req.params.number)
     }
 
     @Get("/hi")
